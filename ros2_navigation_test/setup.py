2a22144703175df1a2947edb0ac207a2b24214ac
Takuchan <takuchanpp@gmail.com>
2023-11-03 13:15:31 +0900

    Rvizとgazeboに表示できるようになった

--- ros2_navigation_test/setup.py
+++ ros2_navigation_test/setup.py
@@ -14,14 +14,14 @@ setup(
         ('share/' + package_name, ['package.xml']),
 
         (os.path.join('share', package_name, 'launch'), glob('launch/*.py')),
-        (os.path.join('share', package_name, 'urdf'), glob('urdf/*')),
+        (os.path.join('share', package_name, 'urdf'), glob('urdf/*.urdf')),
         (os.path.join('share',package_name,'rviz'),glob('rviz/*')),
     ],
     install_requires=['setuptools'],
     zip_safe=True,
     maintainer='tk',
     maintainer_email='takuchanpp@gmail.com',
-    description='TODO: Package description',
+    description='独自urdfファイルを用いてRvizとGazebo上にオブジェクト配置してプログラムを実行する',
     license='TODO: License declaration',
     tests_require=['pytest'],
     entry_points={
